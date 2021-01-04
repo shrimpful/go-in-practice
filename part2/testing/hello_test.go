a907c1c26d1c8ffc2e7661c189a6cc4b5da9d265
Maxim Rudenko <Rudenko.Maksim@wildberries.ru>
2021-01-04 16:22:47 +0300

    added dir testing

---
+++ part2/testing/hello_test.go
@@ -0,0 +1,9 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	if v:=Hello();v!="hello"{
+		t.Errorf("Expected 'hello', but got '%s'",v)
+	}
+}
---
+++ part2/testing/msg.go
@@ -0,0 +1,17 @@
+package main
+
+type Message struct {
+
+}
+
+func (m *Message) Send(email,subject string,body []byte)error{
+	return nil
+}
+
+type Messager interface {
+	Send(email,subject string,body []byte)error
+}
+
+func Alert(m Messager, problem []byte)error {
+	return m.Send("noc@example.com","Critical Error",problem)
+}
