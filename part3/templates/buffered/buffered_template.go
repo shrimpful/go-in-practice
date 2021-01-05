f7f48dd0051631ea28e408934cd5e946e0190cec
Maxim Rudenko <Rudenko.Maksim@wildberries.ru>
2021-01-05 14:41:25 +0300

    added dir templates

---
+++ part3/templates/buffered/buffered_template.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+var t *template.Template
+
+func init() {
+	t=template.Must(template.ParseFiles("simple.html"))
+}
+type Page struct {
+	Title,Content string
+}
+
+func displayPage(w http.ResponseWriter, r *http.Request) {
+	p:=&Page{
+		Title: "An Example",
+		Content:"Have fun stormin' da castle.",
+	}
+	var b bytes.Buffer
+	err:=t.Execute(&b,p)
+	if err != nil {
+		fmt.Fprint(w,"A error occurred.")
+		return
+	}
+	b.WriteTo(w)
+}
+
+func main() {
+	http.HandleFunc("/",displayPage)
+	http.ListenAndServe(":8080",nil)
+}
---
+++ part3/templates/cache_template.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var t=template.Must(template.ParseFiles("simple.html"))
+type Page struct {
+	Title,Content string
+}
+
+func displayPage(w http.ResponseWriter, r *http.Request) {
+	p:=&Page{
+		Title: "An Example",
+		Content: "Have fun stormin' da castle.",
+	}
+	t.Execute(w,p)
+}
+func main() {
+	http.HandleFunc("/",displayPage)
+	http.ListenAndServe(":8080",nil)
+}
