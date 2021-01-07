fceb2383c05f034c6c735db01263ef58686429bd
Maxim Rudenko <Rudenko.Maksim@wildberries.ru>
2021-01-07 18:03:40 +0300

    added dir cloud-services

---
+++ part3/Cloud-services/check_dep.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+)
+
+func checkDep(name string) error {
+	if _,err:=exec.LookPath(name); err!=nil{
+		es:="Could not find '%s' in PATH: %s"
+		return fmt.Errorf(es,name,err)
+	}
+	return nil
+}
+
+func main() {
+	err:=checkDep("fortune")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("Time to get your fortune")
+}
